2d7d64101500c8d6cbcd1d55fa6773a924041f7b
Romain <romain.badino@hotmail.fr>
2022-05-25 09:27:57 +0200

    scéance 25 mai 22 - 2nd commit

---
+++ whereismytag/StopView/StopView.swift
@@ -0,0 +1,20 @@
+//
+//  StopView.swift
+//  whereismytag
+//
+//  Created by badinor on 25/05/2022.
+//
+
+import SwiftUI
+
+struct StopView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct StopView_Previews: PreviewProvider {
+    static var previews: some View {
+        StopView()
+    }
+}
